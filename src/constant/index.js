f806ae3c318321ce8f01a076177d04c88946b7ee
Stas <stasdev@empireads.dev>
2024-08-23 14:26:56 +0300

    change url

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,7 +1,7 @@
 //Local Server
 // export const LOCAL_SERVER = "http://192.168.3.176:3005/" // Local server
 // export const LOCAL_SERVER = "http://192.168.3.174:3002/" // Local server
-export const LOCAL_SERVER = "https://patishon2-9a953cfa7090.herokuapp.com/" // Dell server
+export const LOCAL_SERVER = "https://api.patishon.com/" // Dell server
 
 // SERVER
 
